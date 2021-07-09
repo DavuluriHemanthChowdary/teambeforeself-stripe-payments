ccd2a74eea1c442189d46fefa4c21243888c6a0b
Davuluri Hemanth Chowdary <hemanth.davuluri@minutestoseconds.com>
2021-07-09 21:15:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.get("/", (request, response) => {
   return response.send("hello");
 });
 
-app.post("/monthly-subscription", async (req, res) => {
+app.get("/monthly-subscription", async (req, res) => {
   const session = await stripe.checkout.sessions.create({
     payment_method_types: ["card"],
     line_items: [
@@ -36,7 +36,7 @@ app.post("/monthly-subscription", async (req, res) => {
   res.redirect(303, session.url);
 });
 
-app.post("/yearly-subscription", async (req, res) => {
+app.get("/yearly-subscription", async (req, res) => {
   const session = await stripe.checkout.sessions.create({
     payment_method_types: ["card"],
     line_items: [
